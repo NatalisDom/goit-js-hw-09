f10cfa9b4f52a25ea6a9c6dfb5e6ebfc20affc0f
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-01-28 03:12:43 +0200

    commit
    01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+const elBody = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', startBtnColor);
+stoptBtn.addEventListener('click', stopBtnColor);
+
+stoptBtn.disabled = true;
+
+const INTERVAL_DELAY = 1000;
+let timerId = null;
+
+function startBtnColor() {
+  function colorBody () {
+    elBody.style.backgroundColor = getRandomHexColor();
+  }
+    timerId = setInterval(colorBody, INTERVAL_DELAY);
+
+  startBtn.disabled = true;
+  stoptBtn.disabled = false;
+}
+
+function stopBtnColor() {
+  clearInterval(timerId);
+  stoptBtn.disabled = true;
+  startBtn.disabled = false;
+}
